64e290f7a29da93bc1472fbb073978e044378669
steph_d3r0m <stephane.derom@gmail.com>
2020-04-06 14:05:24 +0200

    un pepin

---
+++ js/diaporama.js
@@ -0,0 +1,34 @@
+//On attend que le DOM soit chargé
+window.onload = () => {
+    //Récupération noeud correspondant au diaporama
+    let diaporama = document.querySelector("header>section");
+    let divGlobe = document.querySelector("header>section>div");
+
+    // //Récupération noeud dot-circle et parent
+    // let dotCircle = document.querySelector(".la-dot-circle");
+    // let circlesParent = dotCircle.parentElement;
+
+    //Création de 3 éléments image 
+
+    for (let i = 2; i <= 3; i++) {
+        let img = document.createElement("img");
+        img.src = "images/index-slide0" + i + ".jpg";
+        diaporama.insertBefore(img, divGlobe);
+        img.style.display = "none";
+    }
+
+    //Initialisation collection éléments img
+    let slides = document.querySelectorAll("header>section>img");
+    let compt = 0;
+
+    setInterval(defilerSlide, 1000);
+}
+
+function defilerSlide() {
+
+    slides[compt % 3].style.display = "none";
+    compt++;
+    slides[compt % 3].style.display = "initial";
+
+
+}
