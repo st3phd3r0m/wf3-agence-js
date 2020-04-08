e6785b9a67a05fef25dfd28012f43e81ec09c5bd
steph_d3r0m <stephane.derom@gmail.com>
2020-04-08 17:01:42 +0200

    version jQuery

--- js/diaporama.js
+++ js/diaporama.js
@@ -1,33 +1,107 @@
+//Variables globales
+let images = ["images/index-slide01.jpg", "images/index-slide02.jpg", "images/index-slide03.jpg"];
+
+let pointeur = 0;
+
 //On attend que le DOM soit chargé
 window.onload = () => {
-    //Récupération noeud correspondant au diaporama
-    let diaporama = document.querySelector("header>section");
-    // console.log(diaporama)
-    let divGlobe = document.querySelector("header>section>div");
 
-    // //Récupération noeud dot-circle et parent
-    // let dotCircle = document.querySelector(".la-dot-circle");
-    // let circlesParent = dotCircle.parentElement;
+    //"mise à jour" du nombre de cercle dans le diaporama
+    miseAJourCercle();
 
-    //Création de 3 éléments image 
+    //Lancement diaporama
+    let period = 1000;
+    let interVal = setInterval(defiler, period);
 
-    for (let i = 2; i <= 3; i++) {
-        let img = document.createElement("img");
-        img.src = "images/index-slide0" + i + ".jpg";
-        diaporama.insertBefore(img, divGlobe);
-        img.style.display = "none";
+    //Ecouteur d'évenement pour interrompre le diaporama 
+    let section = document.querySelector("header>section");
+    section.addEventListener("mouseover", function () {
+        clearInterval(interVal);
+    });
+
+    //Ecouteur d'évenement pour relancer le diaporama
+    section.addEventListener("mouseout", function () {
+        interVal = setInterval(defiler, period);
+    });
+
+    //Diaporama manuel : image de droite
+    let plus = document.querySelector(".la-chevron-right");
+    plus.addEventListener("click", defiler);
+
+    //Diaporama manuel : image de gauche
+    let moins = document.querySelector(".la-chevron-left");
+    moins.addEventListener("click", defileMoins);
+
+} // Fin windows.onload
+
+function miseAJourCercle() {
+    // Supprimer circles et dot-circles
+    let circleParent = document.querySelector("section>div:last-child");
+    circleParent.innerHTML = "";
+    //Créer autant de cercles qu'il y a d'images
+    for (let compteur = 0; compteur < images.length; compteur++) {
+        let circle = document.createElement("i");
+        if (compteur == 0) {
+            circle.classList.add("las", "la-dot-circle");
+        } else {
+            circle.classList.add("las", "la-circle");
+        }
+
+        circleParent.appendChild(circle);
     }
+}
 
-    //Initialisation collection éléments img
-    let slides = document.querySelectorAll("header>section>img");
-    let compt = 0;
-    setInterval(defilerSlide, 1000) ;
 
-    function defilerSlide() {
-        slides[compt % 3].style.display = "none";
-        compt++;
-        slides[compt % 3].style.display = "initial";
+
+/**
+ * Fait avancer le diaporama d'une image
+ */
+function defiler() {
+
+    //Collection des éléments ronds
+    let circles = document.querySelectorAll("section div:nth-of-type(2)>i");
+
+    //Avant que ne s'incrémente le pointeur, on remplace le rond plein du slide précédent par un rond vide en manipulant la classe correspondante
+    circles[pointeur].classList.replace("la-dot-circle", "la-circle");
+
+    // On incrémente le tableau
+    pointeur++;
+    // Si le pointeur dépasse le bout du tableau
+    if (pointeur == images.length) {
+        // on réinitialise le pointeur
+        pointeur = 0;
     }
+    //Récupération noeud correspondant au diaporama : on va chercher l'image
+    let diaporama = document.querySelector("header>section>img");
+    diaporama.setAttribute("src", images[pointeur]);
 
-}// Fin windows.onload
+    //Le pointeur étant à jour, on remplace le rond vide du slide présent par un rond plein en manipulant la classe correspondante
+    circles[pointeur].classList.replace("la-circle", "la-dot-circle");
+}
 
+
+/**
+ * Faire reculer le diaporama d'une image
+ */
+function defileMoins() {
+
+    //Collection des éléments ronds
+    let circles = document.querySelectorAll("section div:nth-of-type(2)>i");
+
+    //Avant que ne décrémente le pointeur, on remplace le rond plein du slide précédent par un rond vide en manipulant la classe correspondante
+    circles[pointeur].classList.replace("la-dot-circle", "la-circle");
+
+    // On décrémente le tableau
+    pointeur--;
+    // Si le pointeur dépasse le début du tableau
+    if (pointeur == -1) {
+        // on réinitialise le pointeur
+        pointeur = images.length - 1;
+    }
+    //Récupération noeud correspondant au diaporama : on va chercher l'image
+    let diaporama = document.querySelector("header>section>img");
+    diaporama.setAttribute("src", images[pointeur]);
+
+    //Le pointeur étant à jour, on remplace le rond vide du slide présent par un rond plein en manipulant la classe correspondante
+    circles[pointeur].classList.replace("la-circle", "la-dot-circle");
+}
--- js/diaporamajQuery.js
+++ js/diaporamajQuery.js
@@ -3,55 +3,46 @@ let images = ["images/index-slide01.jpg", "images/index-slide02.jpg", "images/in
 
 let pointeur = 0;
 
+let interval;
+
+let period;
+
 //On attend que le DOM soit chargé
-window.onload = () => {
+$(function () {
 
     //"mise à jour" du nombre de cercle dans le diaporama
     miseAJourCercle();
 
     //Lancement diaporama
-    let period = 1000;
-    let interVal = setInterval(defiler, period);
+    period = 1000;
+    interval = setInterval(defiler, period);
 
     //Ecouteur d'évenement pour interrompre le diaporama 
-    let section = document.querySelector("header>section");
-    section.addEventListener("mouseover", function () {
-        clearInterval(interVal);
-    });
-
-    //Ecouteur d'évenement pour relancer le diaporama
-    section.addEventListener("mouseout", function () {
-        interVal = setInterval(defiler, period);
-    });
+    $("header>section").on("mouseover", arretDefile).on("mouseout", repriseDefile);
 
     //Diaporama manuel : image de droite
-    let plus = document.querySelector(".la-chevron-right");
-    plus.addEventListener("click", defiler);
+    $(".la-chevron-right").on("click", defiler);
 
     //Diaporama manuel : image de gauche
-    let moins = document.querySelector(".la-chevron-left");
-    moins.addEventListener("click", defileMoins);
+    $(".la-chevron-left").on("click", defileMoins);
 
-} // Fin windows.onload
+}); // Fin windows.onload
+
 
 function miseAJourCercle() {
     // Supprimer circles et dot-circles
-    let circleParent = document.querySelector("section>div:last-child");
-    circleParent.innerHTML = "";
+    let points = $("header>section>div:last-child");
+    points.empty();
+
     //Créer autant de cercles qu'il y a d'images
     for (let compteur = 0; compteur < images.length; compteur++) {
-        let circle = document.createElement("i");
         if (compteur == 0) {
-            circle.classList.add("las", "la-dot-circle");
+            points.append("<i class='las la-dot-circle'></i>");
         } else {
-            circle.classList.add("las", "la-circle");
+            points.append("<i class='las la-circle'></i>");
         }
-
-        circleParent.appendChild(circle);
     }
 }
-
-
 
 /**
  * Fait avancer le diaporama d'une image
@@ -59,11 +50,11 @@ function miseAJourCercle() {
 function defiler() {
 
     //Collection des éléments ronds
-    let circles = document.querySelectorAll("section div:nth-of-type(2)>i");
+    let circles = $("section div:nth-of-type(2)>i");
 
     //Avant que ne s'incrémente le pointeur, on remplace le rond plein du slide précédent par un rond vide en manipulant la classe correspondante
-    circles[pointeur].classList.replace("la-dot-circle", "la-circle");
-
+    $(circles[pointeur]).removeClass("la-dot-circle").addClass("la-circle");
+    circles[pointeur].classList.replace("la-dot-circle","la-circle");
     // On incrémente le tableau
     pointeur++;
     // Si le pointeur dépasse le bout du tableau
@@ -72,36 +63,38 @@ function defiler() {
         pointeur = 0;
     }
     //Récupération noeud correspondant au diaporama : on va chercher l'image
-    let diaporama = document.querySelector("header>section>img");
-    diaporama.setAttribute("src", images[pointeur]);
+    $("header>section>img").attr("src", images[pointeur]);
 
     //Le pointeur étant à jour, on remplace le rond vide du slide présent par un rond plein en manipulant la classe correspondante
-    circles[pointeur].classList.replace("la-circle", "la-dot-circle");
+    circles[pointeur].classList.replace("la-circle","la-dot-circle");
 }
 
-
-/**
- * Faire reculer le diaporama d'une image
- */
 function defileMoins() {
 
     //Collection des éléments ronds
-    let circles = document.querySelectorAll("section div:nth-of-type(2)>i");
+    let circles = $("section div:nth-of-type(2)>i");
 
     //Avant que ne décrémente le pointeur, on remplace le rond plein du slide précédent par un rond vide en manipulant la classe correspondante
-    circles[pointeur].classList.replace("la-dot-circle", "la-circle");
+    $(circles[pointeur]).removeClass("la-dot-circle").addClass("la-circle");
 
     // On décrémente le tableau
     pointeur--;
     // Si le pointeur dépasse le début du tableau
-    if (pointeur == -1) {
+    if (pointeur < 0) {
         // on réinitialise le pointeur
         pointeur = images.length - 1;
     }
     //Récupération noeud correspondant au diaporama : on va chercher l'image
-    let diaporama = document.querySelector("header>section>img");
-    diaporama.setAttribute("src", images[pointeur]);
+    $("header>section>img").attr("src", images[pointeur]);
 
     //Le pointeur étant à jour, on remplace le rond vide du slide présent par un rond plein en manipulant la classe correspondante
-    circles[pointeur].classList.replace("la-circle", "la-dot-circle");
+    $(circles[pointeur]).removeClass("la-circle").addClass("la-dot-circle");
+}
+
+function arretDefile(){
+    clearInterval(interval);
+}
+
+function repriseDefile(){
+    interval = setInterval(defiler, period);
 }
